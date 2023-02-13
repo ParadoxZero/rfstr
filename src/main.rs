04aed675fbe180967d849df68f5536fe5798fa12
Sidhin S Thomas <thomas.sidhin@outlook.com>
2023-02-13 14:57:09 +0530

    Basic substring search implemented.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-mod globals;
-
 use atty::Stream;
 use std::{
     env,
@@ -10,16 +8,34 @@ use std::{
 
 use argust::ParserConfig;
 
+enum MatchMode {
+    CompleteMatch,  // c
+    SubstringMatch, // s
+    FirstSubstring, // f
+    LastSubstring,  // l
+    AllSubstring,   // a
+}
+
 fn main() {
     let args: Vec<OsString> = env::args_os().skip(1).collect();
     let arg_context = argust::parse_args(args.iter(), get_argust_config());
 
-    let lines = get_lines(arg_context);
+    let lines = get_lines(&arg_context);
 
-    println!("{:?}", lines);
+    if lines.is_empty() {
+        eprintln!("No input recieved.");
+    }
+
+    let match_mode = get_mode(&arg_context);
+    let query = get_query(&arg_context);
+
+    lines
+        .iter()
+        .filter_map(|l| if l.contains(&query) { Some(l) } else { None })
+        .for_each(|l| println!("{}", l));
 }
 
-fn get_lines(arg_context: argust::ArgContext) -> Vec<String> {
+fn get_lines(arg_context: &argust::ArgContext) -> Vec<String> {
     let mut lines: Vec<String> = Vec::new();
     if let Some(file_path) = arg_context.contains(Some('p'), Some("path")).1 {
         lines.append(&mut get_lines_from_file(&file_path));
@@ -35,18 +51,38 @@ fn get_lines(arg_context: argust::ArgContext) -> Vec<String> {
     return lines;
 }
 
+fn get_mode(arg_context: &argust::ArgContext) -> MatchMode {
+    if let Some(mode) = arg_context.contains(Some('m'), Some("mode")).1 {
+        match mode.as_ref() {
+            "c" => return MatchMode::CompleteMatch,
+            "s" => return MatchMode::SubstringMatch,
+            "f" => return MatchMode::FirstSubstring,
+            "l" => return MatchMode::LastSubstring,
+            "a" => return MatchMode::AllSubstring,
+            _ => return MatchMode::CompleteMatch,
+        };
+    }
+    return MatchMode::CompleteMatch;
+}
+
+fn get_query(arg_context: &argust::ArgContext) -> String {
+    if let Some(query) = arg_context.contains(Some('q'), Some("query")).1 {
+        return query;
+    }
+    return "".to_string();
+}
+
 fn get_argust_config() -> Option<ParserConfig> {
     let mut parser_config = ParserConfig::new();
     parser_config.parse_tokens.option_key = " ".to_string();
     parser_config.add_parameter('p', "path");
-    parser_config.add_parameter('s', "search");
+    parser_config.add_parameter('q', "query");
     parser_config.add_parameter('m', "mode");
 
     return Some(parser_config);
 }
 
 fn get_lines_from_file(file_name: &str) -> Vec<String> {
-    println!("path: {}", file_name);
     fs::read_to_string(file_name)
         .unwrap_or_else(move |err| panic!("Unable to read from: {}", err))
         .split("\n")
